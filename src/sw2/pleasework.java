845c8301d6079393b43f3deb62dcc622e8c3e3ae
7ssSalha <husseinsoheil1999@hotmail.com>
2020-02-08 10:20:33 +0200

    go

--- src/sw2/pleasework.java
+++ src/sw2/pleasework.java
@@ -5,7 +5,7 @@ public class pleasework {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		print("hussein");
-
+		print("hussein");
 	}
 
 	private static void print(String string) {
