925fd23a4c930dfcf8a3e253a32b21826fd0bedd
7ssSalha <husseinsoheil1999@hotmail.com>
2020-02-08 10:17:22 +0200

    please work

---
+++ src/sw2/pleasework.java
@@ -0,0 +1,16 @@
+package sw2;
+
+public class pleasework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		print("hussein");
+
+	}
+
+	private static void print(String string) {
+		// TODO Auto-generated method stub
+		System.out.print(string);
+	}
+
+}
